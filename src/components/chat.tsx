f0a68e590fc212b236c9f6ce8f68c3e836509dc0
Thiago <thiagosmorato@gmail.com>
2025-04-03 15:58:49 -0300

    configurando ui do projeto

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -33,30 +33,30 @@ export function Chat() {
 
   return (
     <>
-      <div className="flex-1 relative">
+      <div className='flex-1 relative'>
         <div
           ref={containerRef}
-          className="space-y-6 absolute inset-0 overflow-y-scroll scrollbar scrollbar-thumb-rounded-full scrollbar-thumb-zinc-900 scrollbar-track-transparent"
+          className='space-y-6 absolute inset-0 overflow-y-scroll scrollbar scrollbar-thumb-rounded-full scrollbar-thumb-zinc-900 scrollbar-track-transparent'
         >
-          <div className="flex items-start gap-3">
-            <div className="size-7 rounded-md bg-zinc-900 flex items-center justify-center">
-              <User2 className="size-4 text-zinc-100" />
+          <div className='flex items-start gap-3'>
+            <div className='size-7 rounded-md bg-zinc-900 flex items-center justify-center'>
+              <User2 className='size-4 text-zinc-100' />
             </div>
 
-            <div className="flex flex-col gap-4">
-              <div className="flex-1 prose prose-invert prose-zinc prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-base prose-h5:text-sm prose-h6:text-xs">
-                <Markdown>Pergunta</Markdown>
+            <div className='flex flex-col gap-4'>
+              <div className='flex-1 prose prose-invert prose-zinc prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-base prose-h5:text-sm prose-h6:text-xs'>
+                <Markdown># Pergunta</Markdown>
               </div>
             </div>
           </div>
 
-          <div className="flex items-start gap-3">
-            <div className="size-7 rounded-md bg-zinc-900 flex items-center justify-center">
-              <Bot className="size-4 text-zinc-400" />
+          <div className='flex items-start gap-3'>
+            <div className='size-7 rounded-md bg-zinc-900 flex items-center justify-center'>
+              <Bot className='size-4 text-zinc-400' />
             </div>
 
-            <div className="flex flex-col gap-4">
-              <div className="flex-1 prose prose-invert prose-zinc prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-base prose-h5:text-sm prose-h6:text-xs">
+            <div className='flex flex-col gap-4'>
+              <div className='flex-1 prose prose-invert prose-zinc prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-base prose-h5:text-sm prose-h6:text-xs'>
                 <Markdown>Testando resposta</Markdown>
               </div>
             </div>
@@ -65,18 +65,10 @@ export function Chat() {
           <div ref={bottomRef} />
         </div>
 
-        <ChatScrollToBottomButton
-          containerRef={containerRef}
-          scrollRef={bottomRef}
-        />
+        <ChatScrollToBottomButton containerRef={containerRef} scrollRef={bottomRef} />
       </div>
 
-      <MessageInput
-        disabled={false}
-        value={""}
-        onValueChange={() => {}}
-        onSubmit={() => {}}
-      />
+      <MessageInput disabled={false} value={""} onValueChange={() => {}} onSubmit={() => {}} />
     </>
   );
 }
